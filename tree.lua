75d3106ae5253d462eccc1c8c9e3003771213aa9
Владимир Иванов <vivanov879@ya.ru>
2015-10-03 12:01:08 +0300

    no message

--- tree.lua
+++ tree.lua
@@ -327,8 +327,9 @@ for i = 1, 100000 do
   local _, loss_train = optim.adagrad(feval, params, optim_state)
   if i % 100 == 0 then
     print(string.format( 'loss_train = %6.8f, grad_params:norm() = %6.4e, params:norm() = %6.4e', loss_train[1], grad_params:norm(), params:norm()))
-
+    
     tree = trees_dev[math.random(1, #trees_dev)]
+    loss = 0
     forwardProp(tree['root'])
     print(string.format( 'loss_dev = %6.8f', loss))
     
