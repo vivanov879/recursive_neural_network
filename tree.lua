d812b19bb6612dad57e03a594d9b1885851f80e8
Владимир Иванов <vivanov879@ya.ru>
2015-10-03 12:05:11 +0300

    no message

--- tree.lua
+++ tree.lua
@@ -328,7 +328,7 @@ for i = 1, 100000 do
   if i % 100 == 0 then
     print(string.format( 'loss_train = %6.8f, grad_params:norm() = %6.4e, params:norm() = %6.4e', loss_train[1], grad_params:norm(), params:norm()))
     
-    tree = trees_dev[math.random(1, #trees_dev)]
+    tree = trees_dev[1]
     loss = 0
     forwardProp(tree['root'])
     print(string.format( 'loss_dev = %6.8f', loss))
